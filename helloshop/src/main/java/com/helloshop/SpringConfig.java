305522f627684232d866278125397f6342342dd5
Benedict <ailnk@naver.com>
2023-10-23 21:53:03 +0900

    convert to data-jpa

--- helloshop/src/main/java/com/helloshop/SpringConfig.java
+++ helloshop/src/main/java/com/helloshop/SpringConfig.java
@@ -1,12 +1,7 @@
 package com.helloshop;
 
-import com.helloshop.repository.JdbcTemplateMemberRepository;
-import com.helloshop.repository.JpaMemberRepository;
 import com.helloshop.repository.MemberRepository;
-import com.helloshop.repository.MemoryMemberRepository;
 import com.helloshop.service.MemberService;
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,33 +9,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-  public final static int REPO_JDBC_TEMPLATE = 1;
-  public final static int REPO_JPA = 2;
-
-  public int REPO = REPO_JPA;
-
-  private final DataSource dataSource;
-  private final EntityManager em;
+  private final MemberRepository memberRepository;
 
   @Autowired
-  public SpringConfig(DataSource dataSource, EntityManager em) {
-    this.dataSource = dataSource;
-    this.em = em;
+  public SpringConfig(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
   }
 
   @Bean
   public MemberService memberService() {
-    return new MemberService(memberRepository());
-  }
-
-  @Bean
-  public MemberRepository memberRepository() {
-    if (REPO == REPO_JDBC_TEMPLATE) {
-      return new JdbcTemplateMemberRepository(dataSource);
-    } else if (REPO == REPO_JPA) {
-      return new JpaMemberRepository(em);
-    } else {
-      return new MemoryMemberRepository();
-    }
+    return new MemberService(memberRepository);
   }
 }
