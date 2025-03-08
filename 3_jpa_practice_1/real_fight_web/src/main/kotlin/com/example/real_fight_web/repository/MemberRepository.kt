037515b2242bceb984bf1ce81b80c76c39483832
Benedict <ailnk@naver.com>
2025-03-08 19:14:46 +0900

    [추가] 회원 Repo

--- 3_jpa_practice_1/real_fight_web/src/main/kotlin/com/example/real_fight_web/repository/MemberRepository.kt
+++ 3_jpa_practice_1/real_fight_web/src/main/kotlin/com/example/real_fight_web/repository/MemberRepository.kt
@@ -1,21 +1,29 @@
 package com.example.real_fight_web.repository
 
-import com.example.real_fight_web.domain.Member
-import jakarta.persistence.EntityManager
-import jakarta.persistence.PersistenceContext
-import org.springframework.stereotype.Repository
+import com.example.real_fight_web.domain.*
+import jakarta.persistence.*
+import org.springframework.stereotype.*
 
 @Repository
-class MemberRepository {
+class MemberRepository(
     @PersistenceContext
-    lateinit var em: EntityManager
-
+    private val em: EntityManager
+) {
     fun save(member: Member): Long {
         em.persist(member)
         return member.id
     }
 
-    fun find(id: Long): Member {
-        return em.find(Member::class.java, id)
+    fun findOne(id: Long): Member? = em.find(Member::class.java, id)
+
+    fun findAll(): List<Member> {
+        return em.createQuery("select m from Member m", Member::class.java)
+            .resultList
+    }
+
+    fun findByName(name: String): List<Member> {
+        return em.createQuery("select m from Member m where m.name = :name", Member::class.java)
+            .setParameter("name", name)
+            .resultList
     }
 }
