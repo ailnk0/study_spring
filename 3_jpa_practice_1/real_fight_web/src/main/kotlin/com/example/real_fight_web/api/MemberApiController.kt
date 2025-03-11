b9b815f01b3b419727e642858d67e1fc4623d111
Benedict <ailnk@naver.com>
2025-03-11 23:34:55 +0900

    [추가] 회원 조회 API

--- 3_jpa_practice_1/real_fight_web/src/main/kotlin/com/example/real_fight_web/api/MemberApiController.kt
+++ 3_jpa_practice_1/real_fight_web/src/main/kotlin/com/example/real_fight_web/api/MemberApiController.kt
@@ -9,28 +9,6 @@ import org.springframework.web.bind.annotation.*
 class MemberApiController(
     private val memberService: MemberService
 ) {
-    @PostMapping("/api/v1/member")
-    fun saveMemberV1(@RequestBody @Valid member: Member): SaveMemberResponse {
-        val id = memberService.join(member) ?: throw IllegalStateException("회원 가입에 실패했습니다.")
-        return SaveMemberResponse(id)
-    }
-
-    @PostMapping("/api/v2/member")
-    fun saveMemberV2(@RequestBody @Valid request: SaveMemberRequest): SaveMemberResponse {
-        val id = memberService.join(request.toEntity()) ?: throw IllegalStateException("회원 가입에 실패했습니다.")
-        return SaveMemberResponse(id)
-    }
-
-    @PutMapping("/api/v2/member/{id}")
-    fun updateMemberV2(
-        @PathVariable id: Long,
-        @RequestBody @Valid request: UpdateMemberRequest
-    ): UpdateMemberResponse {
-        memberService.update(id, request.name)
-        val findMember = memberService.findOne(id) ?: throw IllegalStateException("회원 정보가 없습니다.")
-        return UpdateMemberResponse(id, findMember.name)
-    }
-
     data class SaveMemberRequest(
         val name: String
     ) {
@@ -43,6 +21,19 @@ class MemberApiController(
         val id: Long
     )
 
+    @PostMapping("/api/v1/member")
+    fun saveMemberV1(@RequestBody @Valid member: Member): SaveMemberResponse {
+        val id = memberService.join(member) ?: throw IllegalStateException("회원 가입에 실패했습니다.")
+        return SaveMemberResponse(id)
+    }
+
+    @PostMapping("/api/v2/member")
+    fun saveMemberV2(@RequestBody @Valid request: SaveMemberRequest): SaveMemberResponse {
+        val id = memberService.join(request.toEntity()) ?: throw IllegalStateException("회원 가입에 실패했습니다.")
+        return SaveMemberResponse(id)
+    }
+
+
     data class UpdateMemberRequest(
         val name: String
     )
@@ -51,4 +42,30 @@ class MemberApiController(
         val id: Long,
         val name: String
     )
+
+    @PutMapping("/api/v2/member/{id}")
+    fun updateMemberV2(
+        @PathVariable id: Long,
+        @RequestBody @Valid request: UpdateMemberRequest
+    ): UpdateMemberResponse {
+        memberService.update(id, request.name)
+        val findMember = memberService.findOne(id) ?: throw IllegalStateException("회원 정보가 없습니다.")
+        return UpdateMemberResponse(id, findMember.name)
+    }
+
+
+    data class MemberDto(
+        val name: String
+    )
+
+    @GetMapping("/api/v1/member")
+    fun findMembersV1(): List<Member> {
+        return memberService.findMembers()
+    }
+
+    @GetMapping("/api/v2/member")
+    fun findMembersV2(): List<MemberDto> {
+        val members = memberService.findMembers()
+        return members.map { MemberDto(it.name) }
+    }
 }
